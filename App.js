37dec292ec24826791f8e4118f1ae637a8ffc8d4
rcovell <rcovell@rolet.com>
2018-06-18 16:07:56 -0500

    version one of testing camera

--- App.js
+++ App.js
@@ -6,6 +6,9 @@
 
 import React, { Component } from 'react';
 import {
+  AppRegistry,
+  Dimensions,
+  TouchableOpacity,
   Platform,
   StyleSheet,
   Text,
@@ -25,11 +28,30 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+        <RNCamera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style = {styles.preview}
+            // type={RNCamera.Constants.Type.back}
+            type={RNCamera.Constants.Type.front}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            permissionDialogTitle={'Permission to use camera'}
+            permissionDialogMessage={'We need your permission to use your camera phone'}
+        />
+        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
+          <TouchableOpacity
+              onPress={this.takePicture.bind(this)}
+              style = {styles.capture}
+          >
+              <Text style={{fontSize: 14}}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js
+          To get started, edit App.js.
         </Text>
         <Text style={styles.instructions}>
           {instructions}
@@ -37,6 +59,19 @@ export default class App extends Component<Props> {
       </View>
     );
   }
+
+  takePicture = async function() {
+    console.log('takePicture');
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options)
+      console.log(data.uri);
+    }
+  };
+
+  onBarCodeRead = function(data) {
+    console.log(data);
+  };
 }
 
 const styles = StyleSheet.create({
